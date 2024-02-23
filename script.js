b3cf644c836a1a90955f750734b75f50a8837697
we0ut <125557450+we0ut@users.noreply.github.com>
2024-02-23 14:39:26 +0100

    finished mobile and desktop css

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const inputEl = document.querySelector(".form-input");
+const checkboxEl = document.querySelector(".circle");
+const closeEl = document.querySelector(".close-btn");
+const stateEl = document.querySelector(".form-state-btn");
+const clearEl = document.querySelector(".form-state-btn--clear");
